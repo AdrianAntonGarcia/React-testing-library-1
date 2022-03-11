1a642f8b770675bdbc39b46f0703e9e6eab7de7e
Antón García <aantong@minsait.com>
2022-03-11 12:56:03 +0100

    React Code: Options and ScoopOption Components

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Row } from "react-bootstrap";
+import { ScoopOption } from "./ScoopOption";
+import axios from "axios";
 
 export const Options = ({ optionType }) => {
-  return <div></div>;
+  const [items, setItems] = useState([]);
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        // TODO: handle error
+      });
+  }, [optionType]);
+
+  // TODO: replace null with toppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 };
