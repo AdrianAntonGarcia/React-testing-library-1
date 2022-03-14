732a9e8b77c902b755c991805993ebd263d65342
Adrián Antón <adrianagarc@ext.inditex.com>
2022-03-14 08:23:29 +0100

    Using wait findBy to Find Elements that Populate Asynchronously

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 
+import { Options } from "./pages/entry/Options";
 import { SummaryForm } from "./pages/summary/SummaryForm";
 
 function App() {
-  return <SummaryForm />;
+  return (
+    <>
+      <SummaryForm />
+      <br />
+      <Options optionType={"scoops"}></Options>
+    </>
+  );
 }
 
 export default App;
