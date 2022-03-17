8fd691aeb8316a1a5095b8b61e77ccc260933bf6
Adrián Antón <adrianagarc@ext.inditex.com>
2022-03-17 10:05:40 +0100

    Toppings Subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,11 @@
-import { Col } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
+
 import React from "react";
 
-export const ToppingOption = ({ name, imagePath }) => {
+export const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.checked ? 1 : 0);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -9,6 +13,18 @@ export const ToppingOption = ({ name, imagePath }) => {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Check onChange={handleChange} type="checkbox" name={name} />
+        </Col>
+      </Form.Group>
     </Col>
   );
 };
--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -1,29 +1,55 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-library-utils";
 
 import { Options } from "../Options";
-import { OrderDetailsProvider } from "../../../context/OrderDetails";
 import userEvent from "@testing-library/user-event";
 
-test("Update scoop subtotal when scoops changes", async () => {
-  render(<Options optionType={"scoops"}></Options>, {
-    wrapper: OrderDetailsProvider,
-  });
-  // make sure total starts out $0.00
-  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
-  expect(scoopsSubtotal).toHaveTextContent("0.00");
+describe("TotalUpdates", () => {
+  test("Update scoop subtotal when scoops changes", async () => {
+    render(<Options optionType={"scoops"}></Options>);
+    // make sure total starts out $0.00
+    const scoopsSubtotal = screen.getByText("Scoops total: $", {
+      exact: false,
+    });
+    expect(scoopsSubtotal).toHaveTextContent("0.00");
 
-  // update vanilla scoops to 1 and check the subtotal
-  const vanillaInput = await screen.findByRole("spinbutton", {
-    name: "Vanilla",
+    // update vanilla scoops to 1 and check the subtotal
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla",
+    });
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, "1");
+    expect(scoopsSubtotal).toHaveTextContent("2.00");
+    // update chocolate scoops to 2 and check subtotal
+    const chocolateInput = await screen.findByRole("spinbutton", {
+      name: "Chocolate",
+    });
+    userEvent.clear(chocolateInput);
+    userEvent.type(chocolateInput, "2");
+    expect(scoopsSubtotal).toHaveTextContent("6.00");
   });
-  userEvent.clear(vanillaInput);
-  userEvent.type(vanillaInput, "1");
-  expect(scoopsSubtotal).toHaveTextContent("2.00");
-  // update chocolate scoops to 2 and check subtotal
-  const chocolateInput = await screen.findByRole("spinbutton", {
-    name: "Chocolate",
+
+  test("Update toppings subtotal when toppings change", async () => {
+    render(<Options optionType={"toppings"}></Options>);
+    // make sure total starts out $0.00
+    const toppingsTotal = screen.getByText("Toppings total: $", {
+      exact: false,
+    });
+    expect(toppingsTotal).toHaveTextContent("0.00");
+    // add cherries and check subtotal
+    const cherriesCheckbox = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+    userEvent.click(cherriesCheckbox);
+    expect(toppingsTotal).toHaveTextContent("1.50");
+    // add hot fudge and check subtotal
+    const hotFudgeCheckbox = screen.getByRole("checkbox", {
+      name: "Hot fudge",
+    });
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent("3.00");
+
+    // remove hot fudge and check subtotal
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent("1.50");
   });
-  userEvent.clear(chocolateInput);
-  userEvent.type(chocolateInput, "2");
-  expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
